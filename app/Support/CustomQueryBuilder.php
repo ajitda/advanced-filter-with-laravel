8e10e27c8d8998e37f91ba60bab4ae417335184b
ajitdas <ajitdas2900@gmail.com>
2018-08-08 22:12:59 +0600

    bug fixed in custom query builder

--- app/Support/CustomQueryBuilder.php
+++ app/Support/CustomQueryBuilder.php
@@ -7,7 +7,7 @@ class CustomQueryBuilder
     {
         if(isset($data['f'])) {
             foreach ($data['f'] as $filter) {
-                $filter['match'] = isset($data['filter_match']) ? $ $data['filter_match'] : "and";
+                $filter['match'] = isset($filter['filter_match']) ? $ $filter['filter_match'] : "and";
                 $this->makeFilter($query, $filter);
             }
         }
